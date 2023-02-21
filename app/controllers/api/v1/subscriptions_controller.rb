ba0761e5f545d10645263c11a97e13532b430386
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-21 15:18:04 -0600

    Comment out code for create & destroy

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,22 +1,22 @@
 module Api
 	module V1
 		class SubscriptionsController < ApplicationController
-			before_action :check_params
+			# before_action :check_params, only: [:create]
 
 			def create
-				render json: SubscriptionsSerializer.new(Subscription.create(thing_params))
+				# render json: SubscriptionsSerializer.new(Subscription.create(thing_params))
 			end
 
 			def destroy
-				if Subscription.exists?(params[:id])
-					render json: SubscriptionsSerializer.new(Subscription.delete(params[:id]))
-				else
-					render json: { errors: 'Not Found' }, status: 404
-				end
+				# if Subscription.exists?(params[:id])
+				# 	render json: SubscriptionsSerializer.new(Subscription.delete(params[:id]))
+				# else
+				# 	render json: { errors: 'Not Found' }, status: 404
+				# end
 			end
 
 			def index
-				render json: SubscriptionsSerializer.new(Subscription.all)
+				render json: SubscriptionSerializer.new(Subscription.all)
 			end
 
 			private
@@ -26,8 +26,8 @@ module Api
 				render json: { errors: 'Bad Request' }, status: 400
 			end
 
-			def thing_params
-				params.require(:thing).permit(:name)
+			def subscription_params
+				params.require(:subscription).permit(:name)
 			end
 		end
 	end
