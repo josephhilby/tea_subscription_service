3ee97bdc43e4c0f74d0e87e4c119e3b09c5046da
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-21 15:18:27 -0600

    Add serializers

---
+++ app/serializers/subscription_serializer.rb
@@ -0,0 +1,9 @@
+class SubscriptionSerializer
+	include JSONAPI::Serializer
+
+  set_type :subscription
+  set_id :id
+  attributes :title, :price, :status, :frequency
+  belongs_to :customer, record_type: :customer
+  belongs_to :tea, record_type: :tea
+end
