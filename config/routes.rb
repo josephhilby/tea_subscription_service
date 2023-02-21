ae63a2cd62d41e6ed288fbe0939b2b8942f9bca7
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-21 14:03:31 -0600

    Add required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      get '/subscriptions', to: 'subscription#index'
+      post '/subscriptions', to: 'subscription#create'
+      delete '/subscriptions/:id', to: 'subscription#destroy'
+    end
+  end
 end
