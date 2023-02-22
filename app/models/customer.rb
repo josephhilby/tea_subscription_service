47e0ae72cd5c8bc7c2595f187fda48ec119f9b9e
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-22 09:25:10 -0600

    Add validations and has_secure_password to customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,12 @@
 class Customer < ApplicationRecord
+  include Bcrypt
+
+  validates_presence_of :email
+  validates_presence_of :password, on: :create
+  validates :email, uniqueness: true, presence: true
+
+  has_secure_password
+
   has_many :subscriptions
   has_many :teas, through: :subscriptions
 end
